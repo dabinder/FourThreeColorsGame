b07afd80d7a7db2f384505f79a10cce04f7e1cab
Dan Binder <dabinder@outlook.com>
2017-11-26 22:15:22 -0500

    initial commit

---
+++ FourThreeColorsGame/StartPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FourThreeColorsGame {
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public sealed partial class StartPage : Page {
+		/// <summary>
+		/// initialize start page
+		/// </summary>
+		public StartPage() {
+			this.InitializeComponent();
+		}
+
+		/// <summary>
+		/// click new game button on start page to begin a game
+		/// </summary>
+		/// <param name="sender">source of new game request</param>
+		/// <param name="e">request detail</param>
+		private void OnNewGameClicked(object sender, RoutedEventArgs e) {
+			this.Frame.Navigate(typeof(View.BoardPage));
+		}
+	}
+}
