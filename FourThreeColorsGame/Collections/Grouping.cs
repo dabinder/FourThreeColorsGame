aeeb2ef1bc72ca8f476d8506c6f279021faf8dc8
Dan Binder <dabinder@outlook.com>
2017-12-07 00:43:41 -0500

    modifications to groupedobservablecollection, remove unused usercontrol

--- FourThreeColorsGame/Collections/Grouping.cs
+++ FourThreeColorsGame/Collections/Grouping.cs
@@ -3,12 +3,12 @@ using System.Collections.ObjectModel;
 using System.Linq;
 
 /*
- * Grouping class, taken from https://github.com/mikegoatly/GroupedObservableCollection
+ * grouping, based on https://montemagno.com/enhancing-xamarin-forms-listview-with-grouping/
  */
 
 namespace FourThreeColorsGame.Collections {
 	public class Grouping<TKey, TValue> : ObservableCollection<TValue>, IGrouping<TKey, TValue> {
-		public TKey Key { get; }
+		public TKey Key { get; private set; }
 
 		public Grouping(TKey key) {
 			this.Key = key;
