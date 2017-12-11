2856a69dcc1f15f1219b672345328bac35178554
Dan Binder <dabinder@outlook.com>
2017-12-10 20:20:50 -0500

    add unit test skeleton

---
+++ FourThreeColors.Tests/DashboardTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using FourThreeColorsGame.Framework;
+using FourThreeColorsGame.Models;
+using FourThreeColorsGame.ViewModels;
+using FourThreeColorsGame.Views;
+
+namespace FourThreeColors.Tests {
+	[TestClass]
+	public class DashboardTests {
+		[TestMethod]
+		public void ColorTotalsUpdate() {
+			GameViewModel model = new GameViewModel();
+
+		}
+	}
+}
