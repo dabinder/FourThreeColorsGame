a94fd395f15504d4a6bb7f5ea3edcf12c76c099a
Dan Binder <dbinder9@gmail.com>
2020-08-01 11:44:59 -0400

    display board

---
+++ QuadRow/Views/GameBoard.xaml.cs
@@ -0,0 +1,16 @@
+﻿using QuadRow.ViewModels;
+using System.Windows.Controls;
+
+namespace QuadRow.Views
+{
+	/// <summary>
+	/// Interaction logic for GameBoard.xaml
+	/// </summary>
+	public partial class GameBoard : UserControl
+	{
+		public GameBoard() {
+			InitializeComponent();
+			DataContext = new BoardViewModel();
+		}
+	}
+}
