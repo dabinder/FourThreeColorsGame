794e00dba27d6df2f27d6013d14cb14806beccd7
Dan Binder <dbinder9@gmail.com>
2020-08-24 17:39:57 -0400

    formatting and display tweaks

--- QuadRow/ViewModels/BoardViewModel.cs
+++ QuadRow/ViewModels/BoardViewModel.cs
@@ -20,8 +20,8 @@ namespace QuadRow.ViewModels
 		private void DrawGameBoard() {
 			//build spaces
 			GameBoard.Clear();
-			for (int x = 0; x < Config.BOARD_SIZE; x++) {
-				for (int y = 0; y < Config.BOARD_SIZE; y++) {
+			for (int y = 0; y < Config.BOARD_SIZE; y++) {
+				for (int x = 0; x < Config.BOARD_SIZE; x++) {
 					Coordinates coords = new Coordinates(x, y);
 					GameBoard.Add(coords, new SpaceViewModel() {
 						TestOrigin = coords.ToString()
