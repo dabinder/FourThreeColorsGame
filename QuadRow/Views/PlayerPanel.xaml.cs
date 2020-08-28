a4aa1b04a18d70e87b7189dfbf99991603b57df4
Dan Binder <dbinder@patientkeeper.com>
2020-08-28 17:57:00 -0400

    move drag handling to viewmodel

--- QuadRow/Views/PlayerPanel.xaml.cs
+++ QuadRow/Views/PlayerPanel.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using QuadRow.ViewModels;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace QuadRow.Views {
 	/// <summary>
@@ -7,6 +9,18 @@ namespace QuadRow.Views {
 	public partial class PlayerPanel : UserControl {
 		public PlayerPanel() {
 			InitializeComponent();
+			DataContextChanged += PlayerPanel_DataContextChanged;
+		}
+
+		private void PlayerPanel_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e) {
+			if (DataContext is PlayerViewModel) {
+				PlayerViewModel model = DataContext as PlayerViewModel;
+				foreach (VisiblePiece piece in new VisiblePiece[] { pieceType1, pieceType2, pieceType3 }) {
+					piece.MouseDown += model.PieceMouseDown;
+					piece.MouseMove += model.PieceMouseMove;
+					piece.GiveFeedback += model.PieceGiveFeedback;
+				}
+			}
 		}
 	}
 }
