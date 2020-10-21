7790ea1bbd312377106121166810921f5b159fd0
Dan Binder <dbinder@patientkeeper.com>
2020-10-21 18:09:38 -0400

    unit tests

---
+++ QuadRowTests/Converters/ColorTypeToColorConverterTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuadRow.Converters;
+using QuadRow.Framework;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace QuadRow.Converters.Tests {
+	[TestClass()]
+	public class ColorTypeToColorConverterTests {
+		private ColorTypeToColorConverter converter = new ColorTypeToColorConverter();
+
+		private bool BrushCompare(SolidColorBrush a, SolidColorBrush b) {
+			return a.Color == b.Color && a.Opacity == b.Opacity;
+		}
+
+		[TestMethod]
+		public void ConvertTest_ReturnsColor() {
+			Assert.AreEqual(Colors.Crimson, converter.Convert(ColorType.Color1, typeof(Color), null, CultureInfo.CurrentCulture));
+			Assert.AreEqual(Colors.SpringGreen, converter.Convert(ColorType.Color2, typeof(Color), null, CultureInfo.CurrentCulture));
+			Assert.AreEqual(Colors.SkyBlue, converter.Convert(ColorType.Color3, typeof(Color), null, CultureInfo.CurrentCulture));
+		}
+
+		[TestMethod]
+		public void ConvertTest_ReturnsBrush() {
+			Assert.IsTrue(BrushCompare(new SolidColorBrush(Colors.Crimson), (SolidColorBrush) converter.Convert(ColorType.Color1, typeof(Brush), null, CultureInfo.CurrentCulture)));
+			Assert.IsTrue(BrushCompare(new SolidColorBrush(Colors.SpringGreen), (SolidColorBrush) converter.Convert(ColorType.Color2, typeof(Brush), null, CultureInfo.CurrentCulture)));
+			Assert.IsTrue(BrushCompare(new SolidColorBrush(Colors.SkyBlue), (SolidColorBrush) converter.Convert(ColorType.Color3, typeof(Brush), null, CultureInfo.CurrentCulture)));
+		}
+	}
+}
