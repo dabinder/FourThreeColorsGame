0818c562f2667793e53be4812cfaaedcccdd0978
Dan Binder <dbinder9@gmail.com>
2020-12-19 13:38:57 -0500

    cleanup

--- QuadRow/ViewModels/Player1ViewModel.cs
+++ QuadRow/ViewModels/Player1ViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using QuadRow.Framework;
 
 namespace QuadRow.ViewModels {
-	public class Player1ViewModel : PlayerViewModel {
+	public sealed class Player1ViewModel : PlayerViewModel {
 		public Player1ViewModel() : base("Player 1", InventoryBuilder.InventoryVariant.Variant1) { }
 	}
 }
--- QuadRow/ViewModels/Player2ViewModel.cs
+++ QuadRow/ViewModels/Player2ViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using QuadRow.Framework;
 
 namespace QuadRow.ViewModels {
-	public class Player2ViewModel : PlayerViewModel {
+	public sealed class Player2ViewModel : PlayerViewModel {
 		public Player2ViewModel() : base("Player 2", InventoryBuilder.InventoryVariant.Variant2) { }
 	}
 }
